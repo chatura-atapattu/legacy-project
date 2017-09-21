cdc73a002dd2cce5218060ddb9085c4d0d24d9c1
Chatura Atapattu <chatura.atapattu@gmail.com>
2017-09-21 16:24:20 +0200

    Split the pages into several

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,10 +3,76 @@ class MainController < ApplicationController
 
   def common
     @currentDonationURL = 'https://www.generosity.com/community-fundraising/t-r-a-p-veracity-project-sofia-bulgaria'
+    @sections = [{url: '/initiatives', title:'Initiatives'}, {url: '/aboutus', title: 'About Us'}, {url: '/faq', title: 'FAQ'}] 
   end
 
   def index
-    @sections = [{url: '#section-1', title:'Initiatives'}, {url: '#section-2', title: 'About Us'}, {url: '#section-3', title: 'FAQ'}] 
+  end
+
+  def initiatives
+  end
+
+  def aboutus
+    @profiles = [
+      {
+        photo: 'dorothy.jpg',
+        name: 'Dorothy Srun',
+        location: 'United States'
+      },
+      {
+        photo: 'eric.jpg',
+        name: 'Eric Cheung',
+        location: 'United States'
+      },
+      {
+        photo: 'kyle.jpg',
+        name: 'Kyle Horan',
+        location: 'United States'
+      },
+      {
+        photo: 'michelle.jpg',
+        name: 'Michelle Parry',
+        location: 'United Kingdom'
+      },
+      {
+        photo: 'nathan.jpg',
+        name: 'Nathan Kroop',
+        location: 'United States'
+      },
+      {
+        photo: 'nick.jpg',
+        name: 'Nicholas Tyral',
+        location: 'Austrailia'
+      },
+      {
+        photo: 'olga.jpg',
+        name: 'Olga Kraineva',
+        location: 'United States'
+      },
+      {
+        photo: 'rich.jpg',
+        name: 'Rich McDonald',
+        location: 'United Kingdom'
+      },
+      {
+        photo: 'sangeeta.jpg',
+        name: 'Sangeeta Mulchandani',
+        location: 'Austrailia'
+      },
+      {
+        photo: 'scott.jpg',
+        name: 'Scott Dukette',
+        location: 'United States'
+      },
+      {
+        photo: 'stevie.jpg',
+        name: 'Stevie Kloeber',
+        location: 'United States'
+      }
+    ]
+  end
+
+  def faq
   end
 
   def privacypolicy
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'main#index'
+  get 'initiatives', to: 'main#initiatives'
+  get 'aboutus', to: 'main#aboutus'
+  get 'faq', to: 'main#faq'
   get 'privacypolicy', to: 'main#privacypolicy'
   get 'termsandconditions', to: 'main#termsandconditions'
   get 'blank', to: 'blank#index'
